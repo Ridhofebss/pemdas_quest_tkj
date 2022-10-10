d5d38999cd0dd67bb2f89e5be78ce5880829e10c
Ridhofebss <112526659+Ridhofebss@users.noreply.github.com>
2022-10-10 10:45:28 +0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,8 @@
+nama_hewan = ["kucing", "tikus", "anjing", "capybara"]
+print("nama hewan yang disekitar:",nama_hewan)
+
+nilai_uts = [100, 200, 300, 400]
+print("nilai uts:",nilai_uts)
+
+nama_teman = ["Ipay", "Bilal", "Isal", "Santo"]
+print("nama teman sebangku:", nama_teman)
