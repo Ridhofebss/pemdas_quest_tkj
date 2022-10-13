c6cfe4138855a19e2e1d53811ecb908a5352588c
Ridhofebss <112526659+Ridhofebss@users.noreply.github.com>
2022-10-13 07:50:22 +0700

    Add files via upload

---
+++ python10.2-tkj4.py
@@ -0,0 +1,47 @@
+# Membuat tipe data koleksi
+
+koleksi_data_str = ["Pisang", "Mangga", "Jambu", "Semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("Koleksi data String", koleksi_data_str)
+
+print("Koleksi data Integer", koleksi_data_int)
+
+print("Koleksi data Campuran", koleksi_data_mix)
+
+koleksi_nama_hewan = ["Gajah", "Harimau", "Singa"]
+
+koleksi_nilai_uts = [77,89,90]
+
+koleksi_teman_sebangku = ["Ipay", "Isal", "Santo"]
+
+print("koleksi nama hewan :",koleksi_nama_hewan)
+
+print("koleksi nilai uts :",koleksi_nilai_uts)
+
+print("koleksi teman sebangku :",koleksi_teman_sebangku)
+
+#Tampilkan data koleksi dengan indeks
+
+# Data ke 2 nama hewan
+print("nama hewan data ke-2",koleksi_nama_hewan[1])
+
+# Data pertama nilai uts
+print("nilai uts data ke-1",koleksi_nilai_uts[0])
+
+# Data terakhir teman sebangku
+print("nama teman sebangku data ke-3:",koleksi_teman_sebangku[2])
+
+# Tambahkan 1 data disetiap data koleksi
+'''tulis disini'''
+
+# ubahlah data terakhir nilai uts
+'''tulis disini'''
+
+# ubahlah data ke-3 nama hewan
+koleksi_nama_hewan[2]="Megalodon"
+
+print(koleksi_nama_hewan)
